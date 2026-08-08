882096454fa988aa04a09f0aa0dd5abd93fa1adf
momohamedbnms-beep <mo.mohamedbnms@yahoo.com>
2026-08-08 18:36:18 +0200

    MAJ sw.js : code de configuration + lien magique

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-var V = "stayclean-agenda-cache-v2";
+var V = "stayclean-agenda-cache-v3";
 var CORE = ["./", "./index.html", "./manifest.webmanifest", "./icon-192.png", "./icon-512.png", "./icon-180.png"];
 self.addEventListener("install", function (e) {
   self.skipWaiting();
