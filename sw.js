1345d50b121d093eafa937040146181f18e74d60
momohamedbnms-beep <mo.mohamedbnms@yahoo.com>
2026-08-10 15:39:10 +0200

    v3 : vues Jour/Semaine/Mois, revenus 4 KPI + annuel, vrai logo StayClean

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-var V = "stayclean-agenda-cache-v3";
+var V = "stayclean-agenda-cache-v4";
 var CORE = ["./", "./index.html", "./manifest.webmanifest", "./icon-192.png", "./icon-512.png", "./icon-180.png"];
 self.addEventListener("install", function (e) {
   self.skipWaiting();
