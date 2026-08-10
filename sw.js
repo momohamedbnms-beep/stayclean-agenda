e48abc9c5cbcb27681c6f4355ecdf4448ef1bee7
momohamedbnms-beep <mo.mohamedbnms@yahoo.com>
2026-08-10 20:38:28 +0200

    Module Avis Google (levier n1)

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-var V = "stayclean-agenda-cache-v6";
+var V = "stayclean-agenda-cache-v7";
 var CORE = ["./", "./index.html", "./manifest.webmanifest", "./icon-192.png", "./icon-512.png", "./icon-180.png"];
 self.addEventListener("install", function (e) {
   self.skipWaiting();
