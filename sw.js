5efeff6ff6d6377c3f99226c993c246ca6418f69
momohamedbnms-beep <mo.mohamedbnms@yahoo.com>
2026-09-09 20:24:10 +0200

    sw.js cache v29 (durées, lien résa, publication sûre)

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-var V = "stayclean-agenda-cache-v28";
+var V = "stayclean-agenda-cache-v29";
 var CORE = ["./", "./index.html", "./manifest.webmanifest", "./icon-192.png", "./icon-512.png", "./icon-180.png"];
 self.addEventListener("install", function (e) {
   self.skipWaiting();
