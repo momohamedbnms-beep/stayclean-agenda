18cda2004c269465d4912725a2bf18ae76a841bf
momohamedbnms-beep <mo.mohamedbnms@yahoo.com>
2026-09-13 21:17:09 +0200

    sw v34 : purge le cache après le fix du lien de réservation

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-var V = "stayclean-agenda-cache-v33";
+var V = "stayclean-agenda-cache-v34";
 var CORE = ["./", "./index.html", "./manifest.webmanifest", "./icon-192.png", "./icon-512.png", "./icon-180.png"];
 self.addEventListener("install", function (e) {
   self.skipWaiting();
